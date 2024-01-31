d03fc5f689602e9ff869259fec174764e57801b7
Harsh-git1 <130690167+Harsh-git1@users.noreply.github.com>
2024-01-31 13:38:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-function validEmail(str) {
+function validEmail(email) {
   //your JS code here.
+   var pattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+	if (pattern.test(email)) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 // Do not change the code below.
